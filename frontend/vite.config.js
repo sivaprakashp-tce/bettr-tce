361736920be3bfec6b4bff3ef5a6663b63c1f993
sivaprakashkrp <sivaprakashkrp@gmail.com>
2026-07-05 22:23:38 +0530

    Corrected env in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig, loadEnv} from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: env.VITE_API_URL,
-        changeOrigin: true,
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
-    },
-    allowedHosts: true
-  },
+      allowedHosts: true
+    }
+  };
 })
